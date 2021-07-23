625738b410669abca90fff37e87bcc8d1fc77ad9
ramuninagendra <ramuninaagendra00@gmail.com>
2021-07-23 12:33:25 +0530

    second commit

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -8,6 +8,13 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
+		
+		int i=10000;
+		
+		
+		System.out.println("print value i==="+i);
+		
 	}
 
 }
